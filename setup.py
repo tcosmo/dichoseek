e8dbf4fbc67dd3c406ae9b50cb46faeef17d509e
Tristan Stérin <tristan.sterin@mu.ie>
2023-05-08 19:58:43 +0200

    0.2.3

--- setup.py
+++ setup.py
@@ -1,15 +1,25 @@
 from distutils.core import setup
 
+from setuptools import setup
+
+# read the contents of your README file
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name="dichoseek",  # How you named your package folder (MyLib)
     packages=["dichoseek"],  # Chose the same as "name"
-    version="0.2",  # Start with a small number and increase it with every change you make
+    version="0.2.3",  # Start with a small number and increase it with every change you make
     license="MIT",  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description="Performs binary search in binary files",  # Give a short description about your library
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Tristan Sterin",  # Type in your name
     author_email="tristan@prgm.dev",  # Type in your E-Mail
     url="https://github.com/tcosmo/dichoseek",  # Provide either the link to your github or to your website
-    download_url="https://github.com/tcosmo/dichoseek/archive/refs/tags/v0.2.2.tar.gz",  # I explain this later on
+    download_url="https://github.com/tcosmo/dichoseek/archive/refs/tags/v0.2.3.tar.gz",  # I explain this later on
     keywords=[
         "binary search",
     ],  # Keywords that define your package best
